62d28f2e1a0e35b186255e3169c15f4c3a89e2ae
Jorge <103595844+jorgelucasp42@users.noreply.github.com>
2024-06-24 21:31:01 -0300

    update

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import "dotenv/config";
+import express from "express";
+import path from "path";
 import app from "./src/app.js";
 
-const PORT = 3000;  
+const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`Servidor escutando na porta ${PORT}`);
