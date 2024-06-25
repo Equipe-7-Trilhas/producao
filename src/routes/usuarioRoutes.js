30e4959ca078879b0806655f5118fd9b06d2104a
Jorge <103595844+jorgelucasp42@users.noreply.github.com>
2024-06-25 12:29:00 -0300

    Update usuarioRoutes.js

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -3,8 +3,7 @@ import UsuarioController from "../controllers/usuarioController.js";
 
 const router = express.Router();
 
-// Certifique-se de que os métodos do controlador estão sendo referenciados corretamente
-router.get("/usuarios", UsuarioController.listarUsuarios); // Correção aqui
+router.get("/usuarios", UsuarioController.listarUsuarios); 
 router.get("/usuarios/:id", UsuarioController.listarUsuarioPorId);
 router.post("/usuarios", UsuarioController.cadastrarUsuario);
 router.put("/usuarios/:id", UsuarioController.atualizarUsuario);
